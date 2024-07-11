62518418dea9bdd240f1e379dab73e62c4942a39
SAMIR DHORAN <samirdhoran2000@gmail.com>
2024-07-11 12:41:38 +0530

    port change

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 4000,
     host: true,
   },
   plugins: [react()],
